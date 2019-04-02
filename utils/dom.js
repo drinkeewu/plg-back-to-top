2088ebf1ccdd44e57f7975dfebd077a63835c926
drinkeewu <773119690@qq.com>
2019-04-02 10:58:57 +0800

    fix: utils supplement

---
+++ utils/dom.js
@@ -0,0 +1,34 @@
+/* eslint-disable func-names */
+
+
+/* istanbul ignore next */
+export const on = (function () {
+  if (document.addEventListener) {
+    return function (element, event, handler) {
+      if (element && event && handler) {
+        element.addEventListener(event, handler, false);
+      }
+    };
+  }
+  return function (element, event, handler) {
+    if (element && event && handler) {
+      element.attachEvent(`on${event}`, handler);
+    }
+  };
+}());
+
+/* istanbul ignore next */
+export const off = (function () {
+  if (document.removeEventListener) {
+    return function (element, event, handler) {
+      if (element && event) {
+        element.removeEventListener(event, handler, false);
+      }
+    };
+  }
+  return function (element, event, handler) {
+    if (element && event) {
+      element.detachEvent(`on${event}`, handler);
+    }
+  };
+}());
